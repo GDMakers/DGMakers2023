cbaee4cce98c8e3814682d5f4230c62af7b5cbbd
DGMakers <72908888+GDMakers@users.noreply.github.com>
2025-10-19 21:44:22 +0200

    j

--- pages/proyectos/Conecta37/index.php
+++ pages/proyectos/Conecta37/index.php
@@ -57,7 +57,7 @@
     ?>
 
     <div class="container-fluid hero-conecta py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5 text-center">
+    <div class="container py-5 text-center">
             <h1 class="display-4 text-white mb-3">Proyecto Conecta 37</h1>
             <p class="lead text-white mb-0">Transformaci&oacute;n digital enfocada en la calidad del aprendizaje y con evidencias.</p>
             <div class="d-flex flex-wrap justify-content-center gap-2 mt-4">
